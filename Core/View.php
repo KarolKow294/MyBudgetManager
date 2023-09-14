1bea19a766d58e6d0bc29d4d3f3f1ffdc15c7b0d
KarolKow294 <kowalski.karol294@gmail.com>
2023-09-14 20:37:36 +0200

    Improvement of Home page

--- Core/View.php
+++ Core/View.php
@@ -19,16 +19,16 @@ class View {
     echo static::getTemplate($template, $args);
     }
 
-    public static function getTemplate($template, $args = []) {
-      static $twig = null;
-  
-      if ($twig === null) {
-        $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
-        $twig = new \Twig\Environment($loader);
-        $twig->addGlobal('current_user', \App\Auth::getUser());
-        $twig->addGlobal('flash_messages', \App\Flash::getMessages());
-        }
-  
-      return $twig->render($template, $args);
+  public static function getTemplate($template, $args = []) {
+    static $twig = null;
+
+    if ($twig === null) {
+      $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
+      $twig = new \Twig\Environment($loader);
+      $twig->addGlobal('current_user', \App\Auth::getUser());
+      $twig->addGlobal('flash_messages', \App\Flash::getMessages());
       }
+
+      return $twig->render($template, $args);
+    }
 }
