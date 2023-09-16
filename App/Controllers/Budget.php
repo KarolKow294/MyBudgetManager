bb2b2bd0a3990fc96283a511d27949abae2157fd
KarolKow294 <kowalski.karol294@gmail.com>
2023-09-16 19:13:43 +0200

    Navigation bar for income and expense

---
+++ App/Controllers/Budget.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+class Budget extends \Core\Controller {
+    public function indexAction() {
+        View::renderTemplate('Budget/index.html');
+    }
+
+    public function incomeAction() {
+        View::renderTemplate('Budget/income.html');
+    }
+
+    public function expenseAction() {
+        View::renderTemplate('Budget/expense.html');
+    }
+
+    public function currentMonthAction() {
+        View::renderTemplate('Budget/currentMonth.html');
+    }
+}
